887d210e3d546cce4d1830664c902980338f9012
corinneah <103472876+corinneah@users.noreply.github.com>
2022-11-13 22:15:08 -0500

    Update crontab.py

--- crontab.py
+++ crontab.py
@@ -1,1 +1,26 @@
+#import packages 
+import os
+import sys 
+import time
+import pandas as pd
 
+# get current working directory
+cwd = os.getcwd()
+print(cwd)
+
+#load API data 
+Data = pd.read_json('https://health.data.ny.gov/resource/jbwf-vnai.json')
+Data
+
+#Export to CSV
+Data.to_csv('data/Healthy_Neighborhoods_Program.csv')
+
+# get the current time/ save as string
+CurrentTime = time.time()
+Time_Now = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(CurrentTime))
+print('The Time Now Is: ', Time_Now)
+
+
+# create a new file in the current working directory
+with open(cwd + '/testFile_' + Time_Now + '.txt', 'w') as f:
+    f.write(str(Data))
